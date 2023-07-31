185104a0323a26309af10c10c406bdfe5ced64d8
Absarsk <absarshaikh19@gmail.com>
2023-07-31 22:27:49 +0530

    31 July

---
+++ Forloop.java
@@ -0,0 +1,21 @@
+// import java.util.Scanner;
+
+// public class Forloop {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         int sum= 0;
+//         int i;
+//         for(i=0; i<=n; i++ ){
+//             sum+=i;
+//         }
+//         System.out.println(sum);
+//     }
+// }
+public class Forloop{
+    public static void main(String[] args) {
+        for(int i=0; i<=10; i++){
+            System.out.println("hello");
+        }
+    }
+}
