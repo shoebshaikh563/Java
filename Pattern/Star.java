16b50e0ea0777a3c59293924343fc8b36ea08742
shoebshaikh563 <shoebshaikh5636@gmail.com>
2023-08-02 15:47:58 +0530

    Shoeb

--- Pattern/Star.java
+++ Pattern/Star.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class Star {
     public static void main(String[] args) {
         int n = 4;
+        int e;
         for (int line = 1; line <= n; line++) {
             for (int row = 1; row <= line; row++) {
                 System.out.print(row);
