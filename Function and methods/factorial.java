75406caaccbdb738641c037c9969b6a3f7608153
shoebshaikh563 <shoebshaikh5636@gmail.com>
2023-08-03 15:55:27 +0530

    3aug

---
+++ Function and methods/factorial.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class factorial {
+    public static int fact(int n) {
+        int f = 1;
+        for (int i = 1; i <= n; i++) {
+            f = f * i;
+        }
+        return f;
+    }
+
+    public static void main(String[] args) {
+        System.out.println((fact(4)));
+        int f=1;
+        System.out.println("Enter No: ");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for (int i = 1; i <= n; i++) {
+            f = f * i;
+        }
+        System.out.println(f);
+
+    }
+}
