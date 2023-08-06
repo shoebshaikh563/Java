67a43c40f1b6e67dd58a187f567b757c526106da
shoebshaikh563 <shoebshaikh5636@gmail.com>
2023-08-06 16:17:53 +0530

    4 aug

---
+++ Function and methods/Binomial.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Binomial {
+   
+    public static int factorial(int n) {
+        int f = 1;
+        for (int i = 1; i <= n; i++) {
+            f = f * i;
+        }
+        return f;
+    }
+
+    public static int binCoeff(int n, int r) {
+        int fact_n = factorial(n);
+        int fact_r = factorial(r);
+        int fact_nmr = factorial(n - r);
+        int binCoeff = fact_n / (fact_r * fact_nmr);
+        System.out.println("Binomial Coeficient is: "+binCoeff);
+        return binCoeff;
+    }
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter value of n");
+        int n = scanner.nextInt();
+        System.out.println("Enter value of r");
+        int r = scanner.nextInt();
+        binCoeff(n, r);
+        int hj = factorial(r);
+        System.out.println(hj);
+
+    }
+}
--- Function and methods/factorial.java
+++ Function and methods/factorial.java
@@ -1,24 +1,27 @@
 import java.util.Scanner;
 
 public class factorial {
-    public static int fact(int n) {
+    public static int fact(int n,int j) {
         int f = 1;
         for (int i = 1; i <= n; i++) {
             f = f * i;
         }
+         System.out.println(f);
+         System.out.println(j);
         return f;
     }
 
     public static void main(String[] args) {
-        System.out.println((fact(4)));
+        // System.out.println((fact(4)));
         int f=1;
         System.out.println("Enter No: ");
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        for (int i = 1; i <= n; i++) {
-            f = f * i;
-        }
-        System.out.println(f);
+        int j = scanner.nextInt();
+        int k =fact(n,j);
+    
+       
+       
 
     }
 }
