0cb47ddc39b90fa4ddb62c16be45b62e60003c92
shoebshaikh563 <shoebshaikh5636@gmail.com>
2023-08-13 16:57:39 +0530

    13 aug

--- EvenOdd.java
+++ EvenOdd.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class EvenOdd {
     public static void main(String[] args){
         System.out.println("Eneter number");
-        Scanner sc = new Scanner(System.in);
-        long number = sc.nextLong();
+        Scanner scanner = new Scanner(System.in);
+        long number = scanner.nextLong();
         if(number%2==0){
             System.out.println("even");
         }
